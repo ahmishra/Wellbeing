3f18fdd9dc3380dc2458380f38dfeab8fb9dcdb9
ahmishra <umasaryan@gmail.com>
2021-08-18 20:36:22 +0530

    Improved Doabetes and Added Symptom Curer

--- main.py
+++ main.py
@@ -1,11 +1,44 @@
 from flask import render_template, request, Flask
 from joblib import load
+from pandas import read_csv
+from numpy import nan
 
+# Global Variables
 model_health_insurance = load("Models/health_insurance_model.joblib")
 scaler_health_insurance = load("Models/health_insurance_scaler.joblib")
+symptom_desc = read_csv("Datasets/symptom_desc.csv")
+symptom_desc['Disease'] = symptom_desc['Disease'].apply(lambda x: x.lower())
+symptom_desc = list(symptom_desc.values)
+symptom_precaution = read_csv("Datasets/symptom_precaution.csv")
+symptom_precaution['Disease'] = symptom_precaution['Disease'].apply(lambda x: x.lower())
+symptom_precaution = list(symptom_precaution.values)
 
+def get_symptom_description(symptom):
+    symptom = symptom.lower()
+    results = []
+    for i in symptom_desc:
+        for j in i:
+            if symptom in str(j):
+                results.append(list(i))
+
+    return results
+
+
+def get_symptom_precaution(symptom):
+    symptom = symptom.lower()
+    results = []
+    for i in symptom_precaution:
+        for j in i:
+            if symptom in str(j):
+                results.append(list(i))
+
+    return results
+
+# Initializing App
 app = Flask(__name__)
 
+
+# App routes and logic
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -14,6 +47,23 @@ def home():
 @app.route("/credits")
 def credits():
     return render_template("credits.html")
+
+
+@app.route("/symptom_describer", methods=["POST", "GET"])
+def symptom_describer():
+    return render_template("symptom_describer.html")
+
+
+@app.route("/symptom_curer", methods=["POST", "GET"])
+def symptom_curer():
+    if request.method == "POST":
+        symptom = str(request.form["disease"]).lower()
+        precuations = get_symptom_precaution(symptom)
+        print(precuations)
+        return render_template("symptom_curer.html", cures=precuations)
+    
+    else:
+        return render_template("symptom_curer.html")
 
 
 @app.route("/heath_insurance_predicter", methods=["GET", "POST"])
@@ -61,6 +111,8 @@ def health_insurance_predicter():
 
     else:
         return render_template("health_insurance_predicter.html")
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
